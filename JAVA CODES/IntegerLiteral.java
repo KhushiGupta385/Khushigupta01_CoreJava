cd69bb0665f7d8de1cc4a79fcf2f95c87f5442c6
Khushi Gupta <khushisatna385@gmail.com>
2026-06-06 22:21:40 +0530

    Format IntegerLiteral.java for consistency

--- JAVA CODES/IntegerLiteral.java
+++ JAVA CODES/IntegerLiteral.java
@@ -1,4 +1,4 @@
-public class IntegerLiteral{
+public class IntegerLiteral {
 	 public static void main(String[] args) {
 	        int a=104;          //Decimal Integer Literal
 	        int b=0150;         //Octal Integer Literal     (iska decimal converion ans hoga)
