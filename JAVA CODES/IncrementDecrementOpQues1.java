60a186f79f2ea0814373579cfe0d92a7f0d02f42
Khushi Gupta <khushisatna385@gmail.com>
2026-06-12 22:04:58 +0530

    Add files via upload

---
+++ JAVA CODES/IncrementDecrementOpQues1.java
@@ -0,0 +1,17 @@
+public class IncrementDecrementOpQues1 {
+    static public void main(String s[]){
+		int a=10,b;
+		//b=++a-a;
+		// b=++a-a+a++;
+		// b=a--+a++-a+a++;
+		// b=--a+a++-a--+a;
+		//b=a++-++a+a--+a--+--a;
+		//b=++a*a--+a--*--a;
+		//b=--a+a++-a;
+		//b=a++-++a-a--;
+		//b=--a*a++/++a-a++-a--;
+		b=--a*a++/a--%a--+--a*a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
