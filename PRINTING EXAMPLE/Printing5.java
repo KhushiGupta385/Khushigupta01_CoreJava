215d114f3277167b65e73e1c9147ecf0d3efcfc3
Khushi Gupta <khushisatna385@gmail.com>
2026-06-16 23:37:24 +0530

    Add files via upload

---
+++ PRINTING EXAMPLE/Printing5.java
@@ -0,0 +1,8 @@
+public class Printing5 {
+    public static void main(String[] args) {
+		//System.out.printf();//error
+		//System.out.format();//error
+		//System.out.print();//error
+		System.out.println();
+	}
+}
