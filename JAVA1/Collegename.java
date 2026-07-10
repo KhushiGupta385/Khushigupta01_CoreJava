c1ec776ff65ad7818f90f39dc4dde3d0df06aeca
Khushi Gupta <khushisatna385@gmail.com>
2026-07-10 18:57:50 +0530

    Delete JAVA1 directory

--- JAVA1/Collegename.java
+++
@@ -1,8 +0,0 @@
-public class Collegename {
-    public static void main (String[]args){
-        String CollegeName = "SAIT";
-        for(int i=0;i<CollegeName.length();i++){
-            System.out.println(CollegeName.charAt(i));
-        }
-    }
-}
--- JAVA1/Myname.java
+++
@@ -1,6 +0,0 @@
-public class Myname {
-    public static void main(String [] args) {
-        System.out.println("khushi gupta");
-        System.out.println("0873CS241088");
-    }
-}
