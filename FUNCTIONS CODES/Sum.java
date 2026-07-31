7fe162d4e078bb85978fdf8382afa09dfec53540
Khushi Gupta <khushisatna385@gmail.com>
2026-07-31 23:16:28 +0530

    Add files via upload

---
+++ FUNCTIONS CODES/Sum.java
@@ -0,0 +1,12 @@
+// Write a method sum(int a, int b) that returns the sum of two numbers.
+public class Sum {
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = sum(10, 20);
+        System.out.println("Sum = " + result);
+    }
+}
