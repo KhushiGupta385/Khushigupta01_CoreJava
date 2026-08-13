0a133d09a22a11459d0daf48e74085706c153d46
Khushi Gupta <khushisatna385@gmail.com>
2026-08-13 17:52:24 +0530

    Add files via upload

--- LOOPS/Armstrong.java
+++ LOOPS/Armstrong.java
@@ -1,21 +1,26 @@
-
+// Print whether a number is Armstrong or not.
 public class Armstrong {
-	public static void main(String[] args) {
-		java.util.Scanner sc=new java.util.Scanner(System.in);
-		
-		System.out.println("Enter a 3-digit number to check Armstrong:");
-		int n=sc.nextInt();
-		int copy=n;
-		int sum=0;
-		while(n!=0) {
-			int r=n%10;
-			sum += r*r*r;
-			n/=10;
-		}
-		if(sum==copy) {
-			System.out.println(copy+" is Armstrong");
-		}else {
-			System.out.println(copy+" is NOT Armstrong");
-		}
-	}
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum = sum + (digit * digit * digit);
+            num = num / 10;
+        }
+
+        if (sum == original) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not an Armstrong Number");
+        }
+
+        sc.close();
+    }
 }
