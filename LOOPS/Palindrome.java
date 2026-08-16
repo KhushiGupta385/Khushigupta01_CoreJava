a262539f65b2f6c11f3ecf9c979f29a83dea183d
Khushi Gupta <khushisatna385@gmail.com>
2026-08-16 15:18:54 +0530

    Add files via upload

---
+++ LOOPS/Palindrome.java
@@ -0,0 +1,26 @@
+// Print whether a number is Palindrome or not.
+public class Palindrome {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+
+        sc.close();
+    }
+}
