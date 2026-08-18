8b133a18717eb0400e95f32f88175298f65a00b2
Khushi Gupta <khushisatna385@gmail.com>
2026-08-18 11:05:48 +0530

    Add files via upload

---
+++ LOOPS/SumOfDigits.java
@@ -0,0 +1,19 @@
+//Print the sum of digits of a number.
+public class SumOfDigits {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+}
